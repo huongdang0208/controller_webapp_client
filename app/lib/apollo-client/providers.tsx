96f5ac1837ca6d589977a78e0f8f4c292704cef6
huongdang0208 <dangthithuhuong02082002@gmail.com>
2024-11-10 11:28:41 +0700

    fix/graphql

--- app/lib/apollo-client/providers.tsx
+++ app/lib/apollo-client/providers.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from "../../lib/redux/store";
 
 const httpLink = createHttpLink({
   uri: "http://51.79.251.117:8080/graphql",
+  credentials: 'include', // Send cookies with requests
 });
 
 export const ApolloClientProvider = ({
